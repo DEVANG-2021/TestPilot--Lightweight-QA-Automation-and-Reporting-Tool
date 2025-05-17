62b0a6a446e0cf339e496a42aa0e1f10d6be701f
Devang Parmar <85817421+DEVANG-2021@users.noreply.github.com>
2025-05-17 18:00:47 -0400

    Update test_runner.py

--- test_runner.py
+++ test_runner.py
@@ -4,6 +4,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
+
+# wait = WebDriverWait(driver, 10)
+# element = wait.until(EC.element_to_be_clickable((By.ID, "submitBtn")))
+# element.click()
+
 def run_tests():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
